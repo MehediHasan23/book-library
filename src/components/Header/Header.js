3760b093de3ea6f30648775a1ba589b8ac3babb5
MehediHasan23 <mehedi12307@gmail.com>
2021-09-26 23:22:19 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   const bookIcon = <FontAwesomeIcon icon={faBook} />
   return (
     <div className='header text-center text-white p-1'>
-      <h1>LIBRARY <span className='text-warning'>{bookIcon}</span></h1>
+      <h1>WANT TO GAIN KNOWLEDGE FOLLOW OUR LIBRARY <span className='text-warning'>{bookIcon}</span></h1>
       <q className='fs-4'>A room without books is like a body without a soul</q>
       <h2>Budget: 50k</h2>
     </div>
